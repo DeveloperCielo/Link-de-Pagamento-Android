753979e0e6ece02b77e5a683f053f5434ba11980
Maurici <m.f.j@hotmail.com>
2020-09-10 18:15:05 -0300

    Added logging interceptor

--- librarycielolinkpagamentos/src/main/java/br/com/cielo/librarycielolinkpagamentos/network/WebClient.kt
+++ librarycielolinkpagamentos/src/main/java/br/com/cielo/librarycielolinkpagamentos/network/WebClient.kt
@@ -1,6 +1,8 @@
 package br.com.cielo.librarycielolinkpagamentos.network
 
+import br.com.cielo.librarycielolinkpagamentos.BuildConfig
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -15,7 +17,15 @@ class WebClient(apiUrl: String) {
 
     fun <T> createService(service: Class<T>): T {
 
+        val logging = HttpLoggingInterceptor()
+        logging.level =
+            if (BuildConfig.DEBUG)
+                HttpLoggingInterceptor.Level.BODY
+            else
+                HttpLoggingInterceptor.Level.NONE
+
         val client = httpClient
+            .addInterceptor(logging)
             .connectTimeout(45, TimeUnit.SECONDS)
             .readTimeout(45, TimeUnit.SECONDS)
             .writeTimeout(45, TimeUnit.SECONDS)
